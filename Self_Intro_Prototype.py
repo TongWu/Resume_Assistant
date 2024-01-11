d4beb2de20b225c4554b88ec29677249bd744010
Ted <ted@wu.engineer>
2024-01-11 20:51:31 +0800

    Fix typo

--- Self_Intro_Prototype.py
+++ Self_Intro_Prototype.py
@@ -7,26 +7,33 @@ from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
+model_selection = os.getenv('GPT_MODEL')
 client = OpenAI(api_key=api_key)
 
 def get_model_version():
-    print("Select the GPT model version: ")
-    print("1. gpt-3.5-turbo-1106        (16,385 tokens)   (Default)")
-    print("2. gpt-4-1106-preview     (128,000 tokens)")
-    models = ['gpt-3.5-turbo-1106', 'gpt-4-1106-preview']
-    try:
-        selection = int(input("Specify the model number: "))
-        if 1 <= selection <= len(models):
-            print(f'Select model {models[selection-1]}')
-            return models[selection - 1]
-        else:
-            print("Invalid selection. Selecting default model (gpt-3.5-turbo-1106).")
+    # TODO: If input with option --model or -m, override model in .env file, and skip the model selection stage
+    if model_selection is not None:
+        print(f"Select the model {model_selection} specify in the .env file. \n")
+        return model_selection
+    else:
+        print("Select the GPT model version: ")
+        print("1. gpt-3.5-turbo-1106        (16,385 tokens)   (Default)")
+        print("2. gpt-4-1106-preview     (128,000 tokens)")
+        models = ['gpt-3.5-turbo-1106', 'gpt-4-1106-preview']
+        try:
+            selection = int(input("Specify the model number: "))
+            if 1 <= selection <= len(models):
+                print(f'Select model {models[selection-1]} \n')
+                return models[selection - 1]
+            else:
+                print("Invalid selection. Selecting default model (gpt-3.5-turbo-1106). \n")
+                return models[0]
+        except ValueError:
+            print("Invalid input. Selecting default model (gpt-3.5-turbo-1106). \n")
             return models[0]
-    except ValueError:
-        print("Invalid input. Selecting default model (gpt-3.5-turbo-1106).")
-        return models[0]
 
 def select_file():
+    # TODO: If user cancel selection, enter the alter command interface
     root = tk.Tk()
     root.withdraw()
     file_path = filedialog.askopenfilename()
@@ -60,11 +67,11 @@ def run_gpt(assistant, thread):
             run_id=run.id
         )
         if not printed:
-            print(f"Run status: {keep_retrieving_run.status}")
+            print("Progressing... ", end='')
             printed = True
 
         if keep_retrieving_run.status == "completed":
-            print("Completed")
+            print("Completed \n")
             break
 
     all_messages = client.beta.threads.messages.list(
@@ -75,10 +82,15 @@ def run_gpt(assistant, thread):
 
 
 def main():
-    print("Please select your resume file: ")
+    # TODO: If input --dryrun or -d, skip the resume selection stage
+    # TODO: If input --file-path or -f, skip the resume selection stage
+    print("Please select your resume file: ", end='')
     file_path = select_file()
+    print(file_path+'\n')
     file_id = upload_file(file_path)
 
+    # TODO: If input with --dryrun or -d, do not create the assistant, give a command interface for more commands
+    # TODO: In the dryrun command interface, 'LIST' to list all existing assistant, select assistant to create a thread
     assistant = client.beta.assistants.create(
         instructions="Now you're an AI assistant helping the interviewer summarize and extend the key points in resume. You need to answer the questions asked by the user based on the resume file provided. Also, the user may ask some general questions, you need to answer these by default model.",
         model=get_model_version(),
@@ -94,10 +106,14 @@ def main():
 
     while True:
         user_message = input("Specify your problem: ")
+        if user_message == 'NEW':
+            main()
+        elif user_message == 'EXIT':
+            break
         ask(thread, user_message)
         all_messages = run_gpt(assistant, thread)
         # print(f"USER: {message.content[0].text.value}")
-        print(f"ASSISTANT: {all_messages.data[0].content[0].text.value}")
+        print(f"ASSISTANT: {all_messages.data[0].content[0].text.value} \n")
 
 
 if __name__ == "__main__":
