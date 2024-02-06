81a84aace473b6f8e0dba1799d8f6659845e10a2
Ted <ted@wu.engineer>
2024-02-07 00:30:33 +0800

    Fix minor bug

--- helper/config.py
+++ helper/config.py
@@ -7,4 +7,4 @@ api_key = os.getenv('OPENAI_API_KEY')
 client = OpenAI(api_key=api_key)
 model_env = os.getenv('GPT_MODEL')
 log_file_path = '../log.csv'
-instruction_path = '../instructions.json'
+instruction_path = './instructions.json'
